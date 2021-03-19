461245e0af98cb1d65237cf8b4842d7b7e2a6620
colibage <78595260+colibage@users.noreply.github.com>
2021-03-19 14:06:55 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,9 @@
 function Deez(){
-    document.documentElement.style.cssText = "--background: #E9E4F8";
-    document.documentElement.style.cssText = "--border: #6D6090";
     document.documentElement.style.cssText = "--text: #474256";
+    document.body.style.cssText += "background-color: var(--light_background);";
 }
 
 function Nutz(){
-    document.documentElement.style.cssText = "--background: #474256";
-    document.documentElement.style.cssText = "--border: #6D6090";
     document.documentElement.style.cssText = "--text: #E9E4F8";
+    document.body.style.cssText += "background-color: var(--dark_background);";
 }
