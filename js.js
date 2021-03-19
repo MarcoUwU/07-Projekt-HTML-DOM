f823d44bd78b63949637bc22f059330e6a05c275
MarcoUwU <58476240+MarcoUwU@users.noreply.github.com>
2021-03-19 13:49:37 +0100

    Update js.js

---
+++ js.js
@@ -0,0 +1,5 @@
+function Deez(){
+    document.documentElement.style.cssText = "--background: #E9E4F8";
+    document.documentElement.style.cssText = "--border: #6D6090";
+    document.documentElement.style.cssText = "--text: #474256";
+}
